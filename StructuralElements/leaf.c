bdfd5d972ee5a24d035c0dc6d63580471fe46b35
aristofanischionis <aristofanischionis@gmail.com>
2018-11-10 01:51:36 +0200

    leaf node almost done correct substring finding and pipe writing reading

--- StructuralElements/leaf.c
+++ StructuralElements/leaf.c
@@ -1,5 +1,6 @@
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <unistd.h>
 #include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -12,85 +13,100 @@
 
 char *fifo = "myfifo";
 
+int findSubstring(char *tobechecked, char *pattern){
+    if(strstr(tobechecked, pattern)){
+        printf("I found the record: %s\n", tobechecked);
+        return 1;
+    }
+    else return 0;
+}
+
 int main ( int argc , char * argv []) {
-    int fd , i , nwrite ;
+    int i , nwrite ;
     char *datafile;
-    int rangeBeg, rangeEnd;
+    int rangeBeg;
     char *pattern;
-    FILE *fpb;
+    FILE *fpb, *fd;
     MyRecord rec;
+    MyRecord rec2 ;
     long lSize;
     int numOfrecords;
-    // char msgbuf[ MSGSIZE +1];
-    if ( argc != 4) { printf( "filename, rangeBeg, rangeEnd, Pattern \n" ); exit(1); }
-    if ( ( fd = open( fifo , O_WRONLY | O_NONBLOCK )) < 0)
-        { perror("fifo open error" ); exit(1); }
+    char tobechecked[sizeof(rec)+1];
+    if ( argc != 5) { 
+        printf( "filename, rangeBeg, numOfrecords, Pattern \n" ); 
+        exit(1); 
+    }
+    if ( ( fd = fopen( fifo , "wb" )) < 0){ 
+        perror("fifo open error" ); 
+        exit(1); 
+    }
 
     datafile = (char *)malloc(strlen(argv[1]) * sizeof(char) + 1);
     strcpy(datafile, argv[1]);
     rangeBeg = atoi(argv[2]);
-    rangeEnd = atoi(argv[3]);
+    numOfrecords = atoi(argv[3]);
     pattern = (char *)malloc(strlen(argv[4]) * sizeof(char) + 1);
     strcpy(pattern, argv[4]);
 
     // read records from file
-
-    fpb = FileRead(datafile);
-        // check number of records
-    fseek (fpb , rangeBeg , rangeEnd);
-    lSize = ftell (fpb);
-    rewind (fpb);
-    numOfrecords = (int)lSize/sizeof(rec);
-
-    printf("Records found in file %d \n", numOfrecords);
-    sleep(5);
+    fpb = fopen (datafile,"rb");
+    if (fpb==NULL) {
+      	printf("Cannot open binary file\n");
+      	return 1;
+   	}
+    // place cursor in the rangeBeg record
+    fseek (fpb , (long)rangeBeg*sizeof(rec) , SEEK_SET);
 
     for (i=0; i<numOfrecords ; i++) {
-            fread(&rec, sizeof(rec), 1, fpb);
-            printf("%ld %s %s  %s %d %s %s %-9.2f\n", \
-            rec.AM, rec.LastName, rec.FirstName, \
-            rec.Street, rec.HouseID, rec.City, rec.postcode, \
-            rec.salary);
-            //  check if pattern exists in file
-
-            if( strstr(rec.AM, pattern) ){
-                // do stuff
-            }
-            else if(strstr(rec.LastName, pattern)) {
-
-            }
-            else if(strstr(rec.FirstName, pattern)){
-                
-            }
-            else if(strstr(rec.Street, pattern)){
-                
-            }
-            else if(strstr(rec.HouseID, pattern)){
-                
-            }
-            else if(strstr(rec.City, pattern)){
-                
-            }
-            else if(strstr(rec.postcode, pattern)){
-                
-            }
-            else if(strstr(rec.salary, pattern)){
-                
-            }
+        fread(&rec, sizeof(rec), 1, fpb);
+        // place the record in a string to check it for substrings
+        int n = sprintf(tobechecked, "%ld %s %s %s %d %s %s %-9.2f\n", \
+        rec.AM, rec.LastName, rec.FirstName, \
+        rec.Street, rec.HouseID, rec.City, rec.postcode, \
+        rec.salary);
+        if( n == 0) {
+            printf("snprintf read 0 chars");
+            exit(1);
         }
+        //  check if pattern exists in file
+        if(findSubstring(tobechecked , pattern) ){
+            // store this record to the pipe
+            rec2.AM = rec.AM;
+            strncpy(rec2.LastName,rec.LastName,20);
+            strncpy(rec2.FirstName,rec.FirstName,20);
+            strncpy(rec2.Street,rec.Street,20);
+            rec2.HouseID = rec.HouseID;
+            strncpy(rec2.City,rec.City,20);
+            strncpy(rec2.postcode,rec.postcode,6);
+            rec2.salary = rec.salary;
+            if (( nwrite = fwrite(&rec2, sizeof(rec2),1,fd) ) == -1){ 
+                perror(" Error in Writing in pipe\n" ); 
+                exit (2) ;
+            }
+        } 
+    }
 
-    
+    fclose(fd);
+    fclose(fpb);
 
-    fclose (fpb);
-    // for (i = 1; i < argc ; i ++) {
-    //     if ( strlen( argv[i] ) > MSGSIZE ) {
-    //         printf( " Message with Prefix %.*s Too long - Ignored \n " ,10 , argv [ i ]) ;
-    //         fflush( stdout );
-    //         continue ;
-    //     }
-    //     strcpy( msgbuf , argv [ i ]) ;
-    //     if (( nwrite = write(fd , msgbuf , MSGSIZE +1) ) == -1)
-    //     { perror(" Error in Writing " ); exit (2) ; }
+// to read from pipe
+    // if ( ( fd = fopen( fifo , "rb" )) < 0){ 
+    //     perror("fifo open error" ); 
+    //     exit(1); 
     // }
+    // fseek (fd , 0 , SEEK_END);
+    // lSize = ftell (fd);
+    // rewind (fd);
+    // numOfrecords = (int) lSize/sizeof(rec);
+    // for (i=0; i<numOfrecords ; i++) {
+    //     fread(&rec, sizeof(rec), 1, fd);
+    //   	printf("%ld %s %s  %s %d %s %s %-9.2f\n", \
+	// 	rec.AM, rec.LastName, rec.FirstName, \
+	// 	rec.Street, rec.HouseID, rec.City, rec.postcode, \
+	// 	rec.salary);
+    // }
+    
+    //     fclose(fd);        
+
     exit(0);
 }
